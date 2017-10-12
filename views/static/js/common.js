c1a0cf2d163099d77e3544619b0615ee9fc0a752
wangruili <2789293971@qq.com>
2017-10-12 09:30:23 +0800

    登录功能完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,15 +1,15 @@
 //在主页面获取cookie
-//   $(function(){
-//     if(location.pathname != "/dashboard/login"){
-//       //获取cookie中用户的相关信息
-//       var userifon= $.cookie('userifon');
-//       //将json字符串转化为json对象
-//       userifon=JSON.parse(userifon);
-//       // var html=template('profile_tpl',userifon)//要添加的模板id,添加的数据
-//       $("#user-ifon").html(template('profile-tpl',userifon));
-//       // $("#user-ifon").html(1111);
-//
-//     }
-//
-//
-//   })
+  $(function(){
+    if(location.pathname != "/dashboard/login"){
+      //获取cookie中用户的相关信息
+      var userifon= $.cookie('userifon');
+      //将json字符串转化为json对象
+      userifon=JSON.parse(userifon);
+      // var html=template('profile_tpl',userifon)//要添加的模板id,添加的数据
+      $("#user-ifon").html(template('profile-tpl',userifon));
+      // $("#user-ifon").html(1111);
+      
+    }
+
+  
+  })
