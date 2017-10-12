d805f444b81b6e6cb40f8ce315dd6ac58b55e125
wangruili <2789293971@qq.com>
2017-10-12 19:30:35 +0800

    1 将登录功能(login.js 和 common.js)模块化 2 添加了只有登录才能访问其它页面的功能

---
+++ views/static/lib/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+require.config({
+  baseUrl:"/views/assets",
+  paths:{
+    "jquery":'jquery/jquery',
+    "cookie":"jquery-cookie/jquery.cookie",
+    "template":"artTemplate/template"
+  }
+})
